a095bd126f8cc06c50c80f0e3f8aa273d6a11eb3
votesystem123 <votesystemproject@gmail.com>
2024-02-05 12:59:46 +0530

    login and register

--- dashboard/models.py
+++ dashboard/models.py
@@ -33,6 +33,8 @@ class AiUser(AbstractBaseUser):
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=100,unique=True)
     password = models.CharField(max_length=100)
+    department = models.CharField(max_length=100)
+    age = models.PositiveIntegerField()
     role = models.CharField(max_length = 20, choices = ROLE_CHOICES, default = 'voter')
     date_joined = models.DateTimeField(default=timezone.now)
     is_admin = models.BooleanField(default=False)
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,19 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from django.contrib.auth import authenticate, login
+
+from dashboard.forms import SignUpForm, LoginForm
 
 # Create your views here.
 
 class RegistrationView(View):
     
     def get(self, request):
+        form = SignUpForm()
+        return render(request, 'dashboard/register.html', {'form': form} )
 
-        return render(request, 'dashboard/register.html' )
+    def post(self, request):
+        try:
+            form = SignUpForm(request.POST)
+            print("form", form)
+            if form.is_valid():
+                user = form.save()
+                return redirect('dashboard:login')
+            return render(request, 'dashboard/register.html', {'form': form, 'error_message': 'invaild data'})
+        except Exception as e:
+            print("error--------------", str(e))
+            return render(request, 'dashboard/register.html', {'form': form, 'error_message': str(e)})
 
 class LoginView(View):
     
     def get(self, request):
+        try:
+            form = LoginForm()
+            return render(request, 'dashboard/login.html',{'form': form} )
+        except Exception as e:
+            return render(request, 'dashboard/login.html', {'form': form, 'error_message': str(e)})
 
-        return render(request, 'dashboard/login.html' )
+    def post(self, request):
+        try:
+            form = LoginForm(request.POST)
+            if form.is_valid():
+                email = form.cleaned_data.get('email')
+                password = form.cleaned_data.get('password')
+                user = authenticate(email=email, password=password)
+                if user is not None :
+                    login(request, user)
+                    return redirect('dashboard:dashboard')
+                else:
+                    error_message = "Invalid username or password."
+                    return render(request, 'dashboard/login.html', {'form': form, 'error_message':error_message})
+            else:
+                return render(request, 'dashboard/login.html', {'form': form, 'error_message': 'invaild data'})
+        except Exception as e:
+            return render(request, 'dashboard/login.html', {'form': form, 'error_message': str(e)})
 
 class DashboardView(View):
     
