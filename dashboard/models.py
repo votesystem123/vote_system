8e46eedda9c33448cfdb7a729d57018c9b93639d
votesystem123 <votesystemproject@gmail.com>
2024-02-05 10:08:55 +0530

    Html template created

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,56 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin, BaseUserManager
+
+from django.utils import timezone
 
 # Create your models here.
+ROLE_CHOICES = (
+    ("voter", "Voter"),
+    ("admin", "Admin"),
+)
+
+class AiUserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None):
+        user = self.create_user(email, password)
+        user.is_admin = True
+        user.role = "admin"
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+class AiUser(AbstractBaseUser):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100,unique=True)
+    password = models.CharField(max_length=100)
+    role = models.CharField(max_length = 20, choices = ROLE_CHOICES, default = 'voter')
+    date_joined = models.DateTimeField(default=timezone.now)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    
+    objects = AiUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    
+    def __str__(self):
+        return self.email
+    
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perms(self, app_label):
+        return self.is_admin
+    class Meta:
+        verbose_name = "Ai User"
+        verbose_name_plural = "Ai Users"
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,47 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+
+class RegistrationView(View):
+    
+    def get(self, request):
+
+        return render(request, 'dashboard/register.html' )
+
+class LoginView(View):
+    
+    def get(self, request):
+
+        return render(request, 'dashboard/login.html' )
+
+class DashboardView(View):
+    
+    def get(self, request):
+        data_labels = ["Label1", "Label2", "Label3"]
+        data_values = [10, 20, 30]
+
+        # Pass the data to the template
+        context = {
+            'labels': data_labels,
+            'values': data_values,
+        }
+        return render(request, 'dashboard/dashboard_admin_home.html',context )
+
+class CandidateView(View):
+    
+    def get(self, request):
+
+        return render(request, 'dashboard/dashboard_admin_candidate.html')
+
+class VotersView(View):
+    
+    def get(self, request):
+
+        return render(request, 'dashboard/dashboard_voters.html')
+
+class CreatePollView(View):
+    
+    def get(self, request):
+
+        return render(request, 'dashboard/dashboard_poll.html')
